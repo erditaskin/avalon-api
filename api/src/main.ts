2968ff18ac651cc65563f5c159daac67d79e9e36
Erdi TAŞKIN <erdi@192.168.1.96>
2024-01-28 15:11:00 +0300

    cors change

--- api/src/main.ts
+++ api/src/main.ts
@@ -15,13 +15,6 @@ async function bootstrap() {
   SwaggerModule.setup('swagger', app, document);
 
   app.useGlobalPipes(new ValidationPipe());
-  app.enableCors({
-    origin: ['vercel.app', 'localhost'],
-    methods: 'GET, HEAD, PUT, POST, DELETE, OPTIONS, PATCH',
-    credentials: true,
-    allowedHeaders:
-      'Origin, X-Requested-With, Content-Type, Accept, Authentication, Access-control-allow-credentials, Access-control-allow-headers, Access-control-allow-methods, Access-control-allow-origin, User-Agent, Referer, Accept-Encoding, Accept-Language, Access-Control-Request-Headers, Cache-Control, Pragma',
-  });
   await app.listen(port, () => {
     console.log('Listening to ', `http://localhost:${port}`);
   });
