b4a35414098178926c69791243e4bebc0b346c5c
Erdi TAŞKIN <erdi@192.168.1.96>
2024-01-28 15:14:26 +0300

    cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -1,10 +1,38 @@
 import { NestFactory } from '@nestjs/core';
 import { CoreModule } from './core.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
-import { ValidationPipe } from '@nestjs/common';
+import { ImATeapotException, ValidationPipe } from '@nestjs/common';
+
+const whitelist = [
+  'http://localhost:3000',
+  'http://localhost:3001',
+  'https://avalon-ui.vercel.app',
+  'vercel.app',
+  'avalon-ui.vercel.app',
+];
 
 async function bootstrap() {
-  const app = await NestFactory.create(CoreModule);
+  const app = await NestFactory.create(CoreModule, {
+    cors: {
+      methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS', 'HEAD'],
+      origin: function (origin, callback) {
+        if (!origin) {
+          callback(null, true);
+          return;
+        }
+        if (
+          whitelist.includes(origin) || // Checks your whitelist
+          !!origin.match(/yourdomain\.com$/) // Overall check for your domain
+        ) {
+          console.log('allowed cors for:', origin);
+          callback(null, true);
+        } else {
+          console.log('blocked cors for:', origin);
+          callback(new ImATeapotException('Not allowed by CORS'), false);
+        }
+      },
+    },
+  });
   const port: number = 3000;
 
   const documentConfig = new DocumentBuilder()
