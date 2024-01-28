1fe3b25d165bb35fd88521f94f9c69367032a6e9
Erdi TAŞKIN <erdi@192.168.1.96>
2024-01-28 17:28:14 +0300

    cors

--- api/src/main.ts
+++ api/src/main.ts
@@ -13,7 +13,11 @@ async function bootstrap() {
     .build();
   const document = SwaggerModule.createDocument(app, documentConfig);
   SwaggerModule.setup('swagger', app, document);
-
+  app.enableCors({
+    allowedHeaders: ['content-type'],
+    origin: 'http://165.232.114.251',
+    credentials: true,
+  });
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(port, () => {
     console.log('Listening to ', `http://localhost:${port}`);
